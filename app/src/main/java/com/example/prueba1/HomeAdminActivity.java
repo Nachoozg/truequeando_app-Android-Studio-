681eb5fc4f72e40c67435c7175d73d172864ae09
Nachoozg <nachozaldo@gmail.com>
2023-05-04 19:06:59 +0200

    A

--- app/src/main/java/com/example/prueba1/HomeAdminActivity.java
+++ app/src/main/java/com/example/prueba1/HomeAdminActivity.java
@@ -1,6 +1,9 @@
 package com.example.prueba1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,14 @@ public class HomeAdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_admin);
+
+        Button IndiceCerrarSesion = findViewById(R.id.cerrar_sesion);
+        IndiceCerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeAdminActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/prueba1/ProductoActivity.java
@@ -0,0 +1,5 @@
+package com.example.prueba1;
+
+public class ProductoActivity {
+
+}
